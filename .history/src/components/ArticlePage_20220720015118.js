3d5337f008454b2bc40e4c1524aac9f6a6ad9511
peterclermy <peterclermy232@gmail.com>
2022-07-20 02:16:13 +0300

    custom-hooks

--- .history/src/components/ArticlePage_20220720015118.js
+++ .history/src/components/ArticlePage_20220720015118.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import useDocumentTitle from "../hooks/useDocumentTitle";
-import useQuery from "../hooks/useQueryAdvanced";
 import { makeEmojiList } from "../utils";
 
 function ArticlePage() {
   // fetch data for a post
   const { id } = useParams();
-  const { data: post, isLoaded } = useQuery(
-    `http://localhost:4000/posts/${id}`
-  );
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [post, setPost] = useState(null);
+
+  const url = `http://localhost:4000/posts/${id}`;
+  useEffect(() => {
+    setIsLoaded(false);
+    fetch(url)
+      .then((r) => r.json())
+      .then((post) => {
+        setPost(post);
+        setIsLoaded(true);
+      });
+  }, [url]);
 
   // set the document title
   const pageTitle = post ? `Underreacted | ${post.title}` : "Underreacted";
-  useDocumentTitle(pageTitle);
+  useEffect(() => {
+    document.title = pageTitle;
+  }, [pageTitle]);
 
   if (!isLoaded) return <h3>Loading...</h3>;
 
